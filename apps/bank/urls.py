39ce99eab951c34e19c8fbab7f2e68d1223c3152
Erivelto Silva <eriveltoclenio@gmail.com>
2024-12-26 17:30:54 +0100

    refactor: transf url

--- apps/bank/urls.py
+++ apps/bank/urls.py
@@ -5,5 +5,5 @@ from .views import AccountCreateView, AccountDetailView, TransferView
 urlpatterns = [
     path("accounts/create/", AccountCreateView.as_view(), name="account-create"),
     path("accounts/<str:account_number>/", AccountDetailView.as_view(), name="account-detail"),
-    path("accounts/transfer/", TransferView.as_view(), name="account-transfer"),
+    path("transfer/", TransferView.as_view(), name="account-transfer"),
 ]
